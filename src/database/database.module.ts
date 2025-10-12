086162447ff87ac25dfce6a6fa03068bee7cbd23
Carlos Zansavio <carlos.zansavio@kantar.com>
2025-10-12 02:39:21 +0100

    Add Prisma and User Module

--- src/database/database.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { dataSourceOptions } from './datasource';
-
-@Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions)],
-  exports: [],
-})
-export class DatabaseModule {}

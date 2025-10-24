6224466358395fbf8dc1e1fbca62da4ff49f7f00
Carlos <carloshlzansavio@gmail.com>
2025-10-24 20:21:57 +0100

    Add user crud

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,12 +1,108 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsArray,
+  IsBoolean,
+  IsEnum,
+  IsOptional,
+  IsString,
+  IsUrl,
+  IsDateString,
+  IsInt,
+  Min,
+  IsNotEmpty,
+} from 'class-validator';
+
+export enum Theme {
+  LIGHT = 'light',
+  DIM = 'dim',
+  DARK = 'dark',
+}
+
+export enum Accent {
+  BLUE = 'blue',
+  YELLOW = 'yellow',
+  PINK = 'pink',
+  PURPLE = 'purple',
+  ORANGE = 'orange',
+  GREEN = 'green',
+}
 
 export class CreateUserDto {
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  bio?: string;
+
   @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   name: string;
 
-  @ApiProperty()
-  email: string;
+  @ApiPropertyOptional({ enum: Theme })
+  @IsOptional()
+  @IsEnum(Theme)
+  theme?: Theme;
+
+  @ApiPropertyOptional({ enum: Accent })
+  @IsOptional()
+  @IsEnum(Accent)
+  accent?: Accent;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsUrl()
+  website?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  location?: string;
 
   @ApiProperty()
-  password: string;
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+
+  @ApiProperty()
+  @IsUrl()
+  photoURL: string;
+
+  @ApiPropertyOptional({ default: false })
+  @IsOptional()
+  @IsBoolean()
+  verified?: boolean;
+
+  @ApiPropertyOptional({ type: [String], default: [] })
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  following?: string[];
+
+  @ApiPropertyOptional({ type: [String], default: [] })
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  followers?: string[];
+
+  @ApiPropertyOptional({ default: 0 })
+  @IsOptional()
+  @IsInt()
+  @Min(0)
+  totalTweets?: number;
+
+  @ApiPropertyOptional({ default: 0 })
+  @IsOptional()
+  @IsInt()
+  @Min(0)
+  totalPhotos?: number;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  pinnedTweet?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsUrl()
+  coverPhotoURL?: string;
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,16 +2,34 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Prisma } from 'generated/prisma';
 
 @Injectable()
 export class UsersService {
   constructor(private prismaService: PrismaService) {}
 
   create(createUserDto: CreateUserDto) {
-    return 'create';
-    // return this.prismaService.user.create({
-    // data: createUserDto,
-    // });
+    const user_data: Prisma.UserCreateInput = {
+      bio: createUserDto.bio,
+      name: createUserDto.name,
+      theme: createUserDto.theme,
+      accent: createUserDto.accent,
+      website: createUserDto.website,
+      location: createUserDto.location,
+      username: createUserDto.username,
+      photoURL: createUserDto.photoURL,
+      verified: createUserDto.verified,
+      following: createUserDto.following,
+      followers: createUserDto.followers,
+      totalTweets: createUserDto.totalTweets,
+      totalPhotos: createUserDto.totalPhotos,
+      pinnedTweet: createUserDto.pinnedTweet,
+      coverPhotoURL: createUserDto.coverPhotoURL,
+    };
+
+    return this.prismaService.user.create({
+      data: user_data,
+    });
   }
 
   findAll() {
@@ -19,14 +37,39 @@ export class UsersService {
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.prismaService.user.findUnique({
+      where: { id: id },
+    });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    const user_data: Prisma.UserUpdateInput = {
+      bio: updateUserDto.bio,
+      name: updateUserDto.name,
+      theme: updateUserDto.theme,
+      accent: updateUserDto.accent,
+      website: updateUserDto.website,
+      location: updateUserDto.location,
+      username: updateUserDto.username,
+      photoURL: updateUserDto.photoURL,
+      verified: updateUserDto.verified,
+      following: updateUserDto.following,
+      followers: updateUserDto.followers,
+      totalTweets: updateUserDto.totalTweets,
+      totalPhotos: updateUserDto.totalPhotos,
+      pinnedTweet: updateUserDto.pinnedTweet,
+      coverPhotoURL: updateUserDto.coverPhotoURL,
+    };
+
+    return this.prismaService.user.update({
+      where: { id: Number(id) },
+      data: user_data,
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    return this.prismaService.user.delete({
+      where: { id: id },
+    });
   }
 }
